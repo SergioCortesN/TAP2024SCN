2685ecdd9928a3bb8d1dad31afecf185ab981d17
SergioCN <sergio.cortes.n290399@gmail.com>
2024-08-27 17:14:30 -0600

    interfaz de usuario principal

--- src/main/java/com/example/tap2024scn/HelloApplication.java
+++ src/main/java/com/example/tap2024scn/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.tap2024scn;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(new Button("holiwis  :3"), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
